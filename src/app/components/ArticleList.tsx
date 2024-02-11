9159386f18550b1744907ef7797c4c381d48bc24
ysk0423 <ssyhrsysk23@gmail.com>
2024-02-11 18:20:05 +0900

    feat: html作成

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const ArticleList = () => {
   return (
-    <div>ArticleList</div>
+    <div>
+      <article>
+        <Link href="#">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt="image"
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div>
+          <Link href="#">Technology</Link>
+          <Link href="#">Next.jsの勉強中</Link>
+          <p>By hogehoge, Published on 2024/02/11</p>
+          <Link href="#">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illum magnam repudiandae neque quaerat, nostrum impedit, eveniet hic assumenda unde mollitia officiis! Omnis accusamus consequatur voluptatum voluptate maiores ipsa quae!
+          </Link>
+          <Link href="#">続きを読む</Link>
+        </div>
+      </article>
+    </div>
   )
 }
 
