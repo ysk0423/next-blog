1242818f83681bfa7c512c6a9cff3b8abca2347c
ysk0423 <ssyhrsysk23@gmail.com>
2024-10-01 08:15:34 +0900

    borderを修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 const Loading = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
-      <div className='w-16 h-16 border-t-14 border-orange-500 rounded-full animate-spin'></div>
+      <div className='w-16 h-16 border-t-4 border-orange-500 rounded-full animate-spin'></div>
     </div>
   )
 }
