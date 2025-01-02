bb6c13042e103fd3b53e7bf6bc57c6fe3b539fa3
ysk0423 <ssyhrsysk23@gmail.com>
2025-01-02 13:48:57 +0900

    コメント追加

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -6,7 +6,7 @@ export const getAllArticles = async (): Promise<Article[]> => {
   if (!res.ok) {
     throw new Error("エラーが発生しました。");
   }
-
+  // Loading確認用
   await new Promise((resolve) => setTimeout(resolve, 1500));
 
   const articles = await res.json();
