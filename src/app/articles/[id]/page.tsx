2f99d4dcded9f6478789f90595440905bb72587d
ysk0423 <ssyhrsysk23@gmail.com>
2025-01-02 15:27:46 +0900

    APIから取得したデータを表示

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -5,15 +5,16 @@ const Article = async ({params}: {params: {id: string}}) => {
   const detailArticle = await getDetailArticle(params.id)
   return (
     <div className="max-w-3x1 mx-auto p-5">
+      {/* 2207863 */}
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+        src={`https://unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt="image"
         width={1280}
         height={300}
       />
-      <h1 className="text-4x1 text-center mb-10 mt-10">ここがタイトルです。</h1>
+      <h1 className="text-4x1 text-center mb-10 mt-10">{detailArticle.title}</h1>
       <div className="text-1g leading-relaxed text-justify">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit harum minima neque quia, obcaecati laudantium corrupti provident ipsum itaque tenetur odio reiciendis praesentium voluptates maxime quasi, nobis earum veritatis culpa!</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   )
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -13,7 +13,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
         <article className="shadow my-4 flex flex-col" key={article.id}>
         <Link href={`articles/${article.id}`} className="hover:opacity-75">
           <Image
-            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
             alt="image"
             width={1280}
             height={300}
